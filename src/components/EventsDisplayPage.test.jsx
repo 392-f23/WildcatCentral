2213532fec1ac671d7ac1be200a1cac5f978f29c
Hunterxia <hunterxia@outlook.com>
2023-11-10 11:21:25 -0600

    Category Filter Test:buggy

--- src/components/EventsDisplayPage.test.jsx
+++ src/components/EventsDisplayPage.test.jsx
@@ -8,7 +8,6 @@ vi.mock("../stores/eventStore", () => ({
   __esModule: true,
   default: vi.fn(() => ({
     categories: ["Information/Mock Session 1", "Information/Mock Session 2"],
-    // Add other states and methods if they are used in the component
   })),
 }));
 
@@ -23,7 +22,6 @@ describe("<EventsDisplay />", () => {
       organizationName: "Org 1",
       categoryNames: ["Information/Mock Session 1"],
     },
-    // Add more mock events if necessary
   ];
 
   beforeEach(() => {
@@ -44,14 +42,11 @@ describe("<EventsDisplay />", () => {
     const select = await screen.findByTestId("category-select");
     userEvent.click(select);
 
-    // Using findByTestId to wait for the category option to appear
     const categoryOption = await screen.findByTestId(
       "category-option-Information-Mock-Session-1"
     );
     userEvent.click(categoryOption);
 
-    // Assertions here...
-    // Depending on the behavior of your component, you may need additional assertions
-    // e.g., to check if the correct events are being displayed after selecting the category
+    expect(screen.getByText("Event 1")).toBeInTheDocument();
   });
 });
